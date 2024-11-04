ca2d8c0df1258ac32deda16155d61d5301571621
H. P. Duan <118118359+hpduan2000@users.noreply.github.com>
2024-11-04 23:05:47 +0800

    update

--- Matlab/file_related/getFolderList.m
+++
@@ -1,8 +0,0 @@
-% School of Civil Engineering, Central South University
-% H.P.Duan, hpduan2000@csu.edu.cn
-% https://www.hpduan.cn
-function list = getFolderList(path)
-    file = dir(path);
-    list = {file.name}';
-    list = list(3:end);
-end
--- Matlab/ground_motion/creatTimeFile.m
+++
@@ -1,9 +0,0 @@
-% School of Civil Engineering, Central South University
-% H.P.Duan, hpduan2000@csu.edu.cn
-% https://www.hpduan.cn
-function Time = creatTimeFile(dt,NPTS,outpath)
-    for i = 1:NPTS
-        Time(i,:) = dt*i;
-    end
-    writematrix(num2str(Time, '%16.6E'),[outpath, 'TIME.txt'])
-end
